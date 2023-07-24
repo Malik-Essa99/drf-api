cfa33b3f219ed3f51959dcc1fa96832d6438c987
Malik-Essa99 <malikhodrob99@gmail.com>
2023-07-24 19:04:42 +0000

    Lab 31 Done

---
+++ README.md
@@ -0,0 +1,10 @@
+### lab 31
+project: Django REST Framework & Docker
+Author: Malik Al Hudrub
+How to initialize/run your application:
++ clone 
++ pip install -r requirements.txt
++ python manage.py runserver
+
+### Testing 
+NA
---
+++ snacks/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Snack
+from rest_framework.generics import ListAPIView,RetrieveAPIView,ListCreateAPIView,RetrieveUpdateAPIView,RetrieveUpdateDestroyAPIView
+from .serializers import SnackSerializer
+# Create your views here.
+
+
+class SnackListView(ListCreateAPIView):
+    queryset = Snack.objects.all()
+    serializer_class = SnackSerializer
+    
+# class SnackListView(ListAPIView):
+#     queryset = Snack.objects.all()
+#     serializer_class = SnackSerializer
+
+class SnackDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Snack.objects.all()
+    serializer_class = SnackSerializer
